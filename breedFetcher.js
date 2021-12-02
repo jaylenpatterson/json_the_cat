f13451ef7d1932d39f2c95dfcc7174390c7745a5
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-12-02 20:55:08 +0000

    function fetches and prints description to console

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,19 @@
 const request = require('request');
-//https://api.thecatapi.com/v1/breeds/search?q=....
+const breedName = process.argv.slice(2);
+const fetcher = function() {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, function(error, response, body) {
+    if (error) {
+      console.log("invalid request");
+      return;
+    }
+    const data = JSON.parse(body);
+    const obj = data[0];
+    if (obj) {
+      console.log(obj.description);
+    } else {
+      console.log("OOPS! Breed not found :c");
+    }
+  });
+};
+fetcher();
